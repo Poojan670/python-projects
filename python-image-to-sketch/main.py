10cefd07ec658705ade22305669e0eb9ca450b36
Poojan670 <po0janhunt@gmail.com>
2022-07-17 11:51:31 +0545

    first commit

---
+++ python-image-to-sketch/main.py
@@ -0,0 +1,99 @@
+import cv2
+import matplotlib.pyplot as pic
+
+'''
+    Original Image Setup
+'''
+
+# seaborn matplotlib styling
+pic.style.use('seaborn-darkgrid')
+
+# read image from cv2
+img = cv2.imread("ori.png")
+
+# Set the image to default RGB
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# image resizing
+pic.figure(figsize=(5, 5))
+pic.imshow(img)
+
+# skip the graph axis hover in image
+pic.axis("off")
+
+# image title
+pic.title("Original Image")
+pic.show()
+
+'''
+    Converting Image to GrayScale to decrease the image complexity
+'''
+
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+pic.figure(figsize=(5, 5))
+pic.imshow(gray_img, cmap="gray")
+pic.axis("off")
+pic.title("GrayScale Image")
+pic.show()
+
+'''
+    Inverting the image to study image precision
+'''
+
+img_invert = cv2.bitwise_not(gray_img)
+pic.figure(figsize=(5, 5))
+pic.imshow(img_invert, cmap="gray")
+pic.axis("off")
+pic.title("Inverted Image")
+pic.show()
+
+'''
+    Image smoothing
+'''
+
+img_smoothing = cv2.GaussianBlur(img_invert, (21, 21), sigmaX=0, sigmaY=0)
+pic.figure(figsize=(5, 5))
+pic.imshow(img_smoothing, cmap="gray")
+pic.axis("off")
+pic.title("Smoothen Image")
+pic.show()
+
+'''
+    Image Finalization
+'''
+
+final_image = cv2.divide(gray_img, 255 - img_smoothing, scale=255)
+pic.figure(figsize=(8, 8))
+pic.imshow(final_image, cmap="gray")
+pic.axis("off")
+pic.title("Final Sketch Image")
+pic.show()
+
+pic.figure(figsize=(20, 20))
+pic.figure('Py-Sketch')
+pic.subplot(1, 5, 1)
+pic.imshow(img)
+pic.axis("off")
+pic.title("Original Image")
+pic.subplot(1, 5, 2)
+pic.imshow(gray_img, cmap="gray")
+pic.axis("off")
+pic.title("GrayScale Image")
+pic.subplot(1, 5, 3)
+pic.imshow(img_invert, cmap="gray")
+pic.axis("off")
+pic.title("Inverted Image")
+pic.subplot(1, 5, 4)
+pic.imshow(img_smoothing, cmap="gray")
+pic.axis("off")
+pic.title("Smoothen Image")
+pic.subplot(1, 5, 5)
+pic.imshow(final_image, cmap="gray")
+pic.axis("off")
+pic.title("Final Sketch Image")
+
+mng = pic.get_current_fig_manager()
+mng.resize(*mng.window.maxsize())
+pic.show()
+
+
---
+++ python-rick-roll/main.py
@@ -0,0 +1,94 @@
+import sys
+import time
+
+import pygame
+import pyttsx3
+
+pygame.init()
+
+engine = pyttsx3.init()
+
+rate = engine.getProperty('rate')
+
+voices = engine.getProperty('voices')
+
+default = engine.setProperty('rate', '200')
+
+engine.setProperty('volume', '10.0')
+
+
+def male_voice():
+    return engine.setProperty('voice', voices[0].id)
+
+
+def female_voice():
+    return engine.setProperty('voice', voices[1].id)
+
+
+def chorus_rate():
+    chorus = engine.setProperty('rate', '2000')
+    return chorus
+
+
+def get_length():
+    with open('data.txt') as f:
+        return len(f.readlines())
+
+
+def file_voice_over():
+    with open('data.txt') as f:
+        for count, line in enumerate(f):
+            # length = get_length()
+            # for i in range(length):
+            #     print(rick + '' + rick)
+            if count in [range(19, 29), range(36, 54)]:
+                chorus_rate()
+                male_voice()
+                print(rick + '' + rick)
+                engine.say(line)
+            elif count % 2 == 0:
+                male_voice()
+                print(rick + '' + rick)
+                engine.say(line)
+            else:
+                female_voice()
+                print(rick + '' + rick)
+                engine.say(line)
+
+
+rick = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⠿⠟⠛⠻⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣆⣀⣀⠀⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠻⣿⣿⣿⠅⠛⠋⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢼⣿⣿⣿⣃⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣟⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⣛⣫⡄⠀⢸⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡆⠸⣿⣿⣿⡷⠂⠨⣿⣿⣿⣿⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⡇⢀⣿⡿⠋⠁⢀⡶⠪⣉⢸⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⣷⣿⣿⣷⣦⡙⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+
+if __name__ == "__main__":
+    try:
+        music = pygame.mixer.Sound("music.mp3")
+        music.set_volume(0.5)
+        music.play()
+        time.sleep(2)
+        file_voice_over()
+        engine.runAndWait()
+    except KeyboardInterrupt:
+        sys.exit("Process terminated by user, Thank you for using me- Poojan")
+    except IOError as i:
+        sys.exit("Something went wrong, Please try again."
+                 "Issue in : {}".format(i))
+    except Exception as e:
+        sys.exit(f"Error occurred due to :{str(e)}")
